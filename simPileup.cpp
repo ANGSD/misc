033087223a67486310a079dbe09d42739efeaa85
ANGSD <thorfinn.sand@gmail.com>
2014-08-16 20:52:14 +0200

    continued playing

--- simPileup.cpp
+++ simPileup.cpp
@@ -1,3 +1,5 @@
+
+
 /*
   simulate pileupdata, 
   1) simulating genotypes
@@ -11,6 +13,13 @@
 #include <unistd.h>
 #include <cmath>
 #include <cassert>
+#include <vector>
+
+const char * PLP =".pileup";
+const char * GENO =".geno";
+const char * TRUEFREQ =".truefreq";
+
+std::vector<char *> ofiles;
 
 float bfreq[4]={0.25,0.5,0.75,1};
 char bases[5] = "ACGT";
@@ -25,7 +34,20 @@ void norm(T*ary,size_t x){
   for(int i=0;i<x;i++) ary[i] /=ssum;
 }
 
-
+FILE *getFP(char *fname,const char *post){
+  fprintf(stderr,"fnaem:%s post:%s\n",fname,post);
+  unsigned len = strlen(fname)+strlen(post)+1;
+  char *tmp =(char*) malloc(strlen(fname)+strlen(post)+1);
+  snprintf(tmp,len,"%s%s",fname,post);
+  
+  FILE *fp = NULL;
+  if(!((fp=fopen(tmp,"wb")))){
+    fprintf(stderr,"Problem opening file: \'%s\'\n",tmp);
+    exit(0);
+  }
+  ofiles.push_back(tmp);
+  return fp;
+}
 
 
 double simfreq() {
@@ -63,14 +85,14 @@ int pickInterval(int n,float points[4]){
 }
 
 
-void per_ind_hap(char genos,int dep,float err){
+void per_ind_hap(char genos,int dep,float err,FILE *fp){
   //  fprintf(stderr,"[%s] genos:%d dep:%d err:%f\n",__FUNCTION__,genos,dep,err);
   for(int d=0;d<dep;d++){
   
     float r = drand48();
     //fprintf(stderr,"d:%d r:%f err:%f\n",d,r,err);  
     if(r>err)
-      fprintf(stdout,"%c" , bases[genos]);
+      fprintf(fp,"%c" , bases[genos]);
     else{
       char b;
       while(1){
@@ -81,13 +103,26 @@ void per_ind_hap(char genos,int dep,float err){
 	  break;
 	}
       }
-      fprintf(stdout,"%c" , bases[b]);
+      fprintf(fp,"%c" , bases[b]);
     }
   }
-  fprintf(stdout,"\t");
+  fprintf(fp,"\t");
 
 }
 
+void print_info(FILE *fp){
+   fprintf(stderr, "\n");
+    fprintf(stderr, "Usage: simPileup  [options] \n");
+    fprintf(stderr, "Options:\n");
+    fprintf(stderr, "   -p <int>            ploidy\n");
+    fprintf(stderr, "   -i <int>            number of individuals\n");
+    fprintf(stderr, "   -s <int>            number of sites\n");
+    fprintf(stderr, "   -o <filename>       outputfilename\n");
+    fprintf(stderr, "   -v <float>          variability\n");
+    fprintf(stderr, "   -m <int>            mean seq depth\n");
+    fprintf(stderr, "   -e <int>            errorrateh\n");
+    fprintf(stderr, "\n");
+}
 
 int main(int argc,char **argv){
 
@@ -102,17 +137,7 @@ int main(int argc,char **argv){
   float var=0.6;
   float err = 0.00;
   if (argc==1) {
-    fprintf(stderr, "\n");
-    fprintf(stderr, "Usage: simPileup  [options] \n");
-    fprintf(stderr, "Options:\n");
-    fprintf(stderr, "   -p <int>            list of positions or regions\n");
-    fprintf(stderr, "   -i <int>            list of input BAM filenames, one per line [null]\n");
-    fprintf(stderr, "   -s <int>            minQLen\n");
-    fprintf(stderr, "   -o <filename>       base quality threshold\n");
-    fprintf(stderr, "   -v <float>       variability\n");
-    fprintf(stderr, "   -m <int>       mean seq depth\n");
-    fprintf(stderr, "   -e <int>      errorrateh\n");
-    fprintf(stderr, "\n");
+    print_info(stderr);
     return 1;
   }
 
@@ -130,6 +155,13 @@ int main(int argc,char **argv){
     case 'e': err = atof(optarg); break;
     }
   }
+  if(!outname){
+    fprintf(stderr,"Must supply -o\n");
+    print_info(stderr);
+    return 1;
+  }
+  FILE *fp_pileup = getFP(outname,PLP);
+  FILE *fp_geno = getFP(outname,GENO);
   fprintf(stderr,"p:%d i:%d s:%d o:%s m:%d v:%f\n",ploidy,nind,nsites,outname,mean,var);
   size_t dim=ploidy==1?nind+1:2*nind+1;
   fprintf(stderr,"dim:%zu\n",dim);
@@ -138,31 +170,31 @@ int main(int argc,char **argv){
   
   assert(ploidy==1);
   
-  for(int s=1;s<=nsites;s++){
-    fprintf(stdout,"chr1\t%d\t",s);
+  for(int s=1;s<=nsites;s++){//over sites
+    fprintf(fp_pileup,"chr1\t%d\t",s);
     char ref=pickInterval(4,bfreq);//<- 0,1,2,3
     char alt=ref;
+
     if(drand48()<=var){
       char b = pickInterval(4,bfreq);
-      //  fprintf(stderr,"b:%d\n",b);
       alt = b==ref?(b+1)%4:b;//<- 0,1,2,3
     }
-    fprintf(stdout,"%c\t%c\t",bases[ref],bases[alt]);
+
+    fprintf(fp_pileup,"%c\t%c\t",bases[ref],bases[alt]);
    
     float sfreq = simfreq();
-    fprintf(stdout,"%f\t",sfreq);
+    // fprintf(stdout,"%f\t",sfreq);
     int aCount =0;
     for(int n=0;n<nind;n++){
       char b=drand48()>sfreq?ref:alt;
+      fprintf(fp_geno,"%c ",bases[b]);
       if(b!=ref) aCount++;
       int d = rpois(mean);
-      //fprintf(stdout,"%c\t",bases[b]);
-      //exit(0);
-      //fprintf(stderr,"-----b:%d\n",b);
-      per_ind_hap(b,d,err);
-      //fprintf(stderr,"-----\n");
-      //exit(0);
+      per_ind_hap(b,d,err,fp_pileup);
     }
+    
+    fprintf(fp_pileup,"\n");
+    fprintf(fp_geno,"\n");
     truefreq[aCount]++;
   }
   fprintf(stdout,"\n");
@@ -171,6 +203,11 @@ int main(int argc,char **argv){
   for(int i=0;i<dim;i++)
     fprintf(stderr,"%f\t",truefreq[i]);
   fprintf(stderr,"\n");
+  #if 1
+  fprintf(stderr,"Dumping files\n");
+  for(unsigned i=0;i<ofiles.size();i++)
+    fprintf(stderr,"\t%s\n",ofiles[i]);
 
+  #endif
   return 0;
 }
